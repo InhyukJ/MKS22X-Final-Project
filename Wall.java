75ffbf551c44d877aba5c3c39d747a68ff8a545d
Winnie Chen <wchen10@stuy.edu>
2017-06-02 15:30:34 -0400

    started Wall

---
+++ Wall.java
@@ -0,0 +1,25 @@
+public class Wall implements Obj{
+    float x, y, width, height;
+
+    public Wall(boolean topLeft, boolean horizontal){
+	if(topLeft && horizontal){
+	    x = 30.0;
+	    y = 30.0;
+	    width = 650.0;
+	    height = 5.0;
+	}else if(topLeft && !horizontal){
+	    x = 30.0;
+	    y = 30.0;
+	}
+	}else{
+
+	}
+	this.x = x;
+	this.y = y;
+	if(vertical){
+	    width = 
+	}else{
+
+	}
+    }
+}
