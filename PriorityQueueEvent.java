4566bb9567a7ff2b181bfc6d7d968c2a1332cb6d
Winnie Chen <wchen10@stuy.edu>
2017-06-05 15:31:33 -0400

    created file

---
+++ PriorityQueueEvent.java
@@ -0,0 +1,14 @@
+public class PriorityQueueEvent{
+    
+
+    public PriorityQueueEvent(){ //probably needs arguments, like ArrayList of Objs
+	
+    }
+
+    
+
+    
+    public static void main(String[] args){
+
+    }
+}
