fff061deb366260677593b7f8350da76d5bd3873
Winnie Chen <wchen10@stuy.edu>
2017-06-06 15:22:57 -0400

    untested

--- PriorityQueueEvent.java
+++ PriorityQueueEvent.java
@@ -1,11 +1,30 @@
 public class PriorityQueueEvent{
-    
+    private HeapEvent heap;
 
-    public PriorityQueueEvent(){ //probably needs arguments, like ArrayList of Objs
-	
+    public PriorityQueueEvent(){ 
+	//probably needs arguments, like ArrayList of Objs - actually, can deal with adding in the EventListener
+	heap = new HeapEvent(false); //minHeap b/c want shorter distances
     }
 
-    
+    public void add(Event event){
+	heap.add(event);
+    }
+
+    public Event peek(){
+	return heap.peek();
+    }
+
+    public Event next(){
+	return heap.remove();
+    }
+
+    public boolean hasNext(){
+	return heap.getSize() != 0;
+    }
+
+    public String toString(){
+	return heap.toStringTree();
+    }
 
     
     public static void main(String[] args){
