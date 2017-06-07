67c41584eb990171775aab2a0b44eba36c605789
InhyukJ <ijun@stuy.edu>
2017-06-07 01:02:19 -0400

    EventListener compilable

--- EventListener.java
+++ EventListener.java
@@ -9,12 +9,25 @@ public class EventListener {
 
     public void update() {
 	Event pEvt = PQ.peek();
+	Obj Obj1 = pEvt.getObj1();
+	Obj Obj2 = pEvt.getObj2();
 	if (isColliding(pEvt)) {
-	    if (!(pEvt.getObj1).isWall() && !(pEvt.getObj2).isWall()) {
-		(Ball)pEvt.
-	
+	    if (!Obj1.isWall() && !Obj2.isWall()) {
+		((Ball)Obj1).bounceB();
+		((Ball)Obj2).bounceB();
+	    }
+	    else if (!Obj1.isWall()) {
+		if (!((Wall)Obj2).isHorizontal()) ((Ball)Obj1).bounceY();
+		else ((Ball)Obj1).bounceX();
+	    }
+	    else {
+		if (((Wall)Obj1).isHorizontal()) ((Ball)Obj2).bounceY();
+		else ((Ball)Obj2).bounceX();
+	    }
+	}
     }
-
+    
     private boolean isColliding(Event evt) {
 	return evt.distanceObj12() < 5.0f;
     }
+}
